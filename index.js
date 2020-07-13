c36e2656181f5e73d5ecf49a32a8caedbbe856a1
Elizabeth <elizbregman@gmail.com>
2020-07-13 21:26:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   return delete ({}, object, key)
   }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete (object,key)
+}
