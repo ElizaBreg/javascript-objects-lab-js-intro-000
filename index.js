532041e053ccb390806c32b46e21fe09d0757181
Elizabeth <elizbregman@gmail.com>
2020-07-13 21:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function  updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign ({}, object, { [key]: value });
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return Object ({[key]: value});
+  object[key] = value;
+  return object
 }
